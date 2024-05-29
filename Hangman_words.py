b2a4e4102e35a7b544a00ae46ce1a2a168ec27f7
Zahra Sadeghi <thisiszahrasadeghi@gmail.com>
2024-05-29 13:57:44 +0330

    Create Hangman_arts and Hangman_words

---
+++ Hangman_words.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+words = "ant babboon badger bat bear beaver camel cat clam cobra cougar coyote crow deer dog donkey duck eagle ferret fox frog goat goose hawk lion lizard llama mole rat raven rhino shark sheep spider toad turkey turtle wolf wombat zebra"
+words_list = words.split(" ")
+print(words_list)
+
