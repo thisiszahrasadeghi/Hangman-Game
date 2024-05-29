88990864131dcc0523db325534f43f1ad6ae7dd2
Zahra Sadeghi <thisiszahrasadeghi@gmail.com>
2024-05-29 18:25:25 +0330

    Update Hangman_words.py

--- Hangman_words.py
+++ Hangman_words.py
@@ -6,5 +6,4 @@
 
 words = "ant babboon badger bat bear beaver camel cat clam cobra cougar coyote crow deer dog donkey duck eagle ferret fox frog goat goose hawk lion lizard llama mole rat raven rhino shark sheep spider toad turkey turtle wolf wombat zebra"
 words_list = words.split(" ")
-print(words_list)
 
